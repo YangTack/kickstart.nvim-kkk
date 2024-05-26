013cebe1242d7800fde81d5052d61e7bec079cee
yangshaobo <yangshaobo1996@gmail.com>
2024-05-26 19:33:30 +0800

    init

--- init.lua
+++ init.lua
@@ -381,49 +381,50 @@ require('lazy').setup({
     config = true,
   },
   -- vim-slime
-  -- {
-  --   'jpalardy/vim-slime',
-  --   init = function()
-  --     vim.g.slime_target = 'neovim'
-  --     vim.g.slime_python_ipython = 1
-  --     vim.g.slime_dispatch_ipython_pause = 100
-  --     vim.g.slime_cell_delimiter = '#\\s\\=%%'
-  --     vim.cmd [[
-  --     function! _EscapeText_quarto(text)
-  --       if slime#config#resolve("python_ipython") && len(split(a:text,"\n")) > 1
-  --         return ["%cpaste -q\n", slime#config#resolve("dispatch_ipython_pause"), a:text, "--\n"]
-  --       else
-  --         let empty_lines_pat = '\(^\|\n\)\zs\(\s*\n\+\)\+'
-  --         let no_empty_lines = substitute(a:text, empty_lines_pat, "", "g")
-  --         let dedent_pat = '\(^\|\n\)\zs'.matchstr(no_empty_lines, '^\s*')
-  --         let dedented_lines = substitute(no_empty_lines, dedent_pat, "", "g")
-  --         let except_pat = '\(elif\|else\|except\|finally\)\@!'
-  --         let add_eol_pat = '\n\s[^\n]\+\n\zs\ze\('.except_pat.'\S\|$\)'
-  --         return substitute(dedented_lines, add_eol_pat, "\n", "g")
-  --       end
-  --     endfunction
-  --     function! _EscapeText_python(text)
-  --       if slime#config#resolve("python_ipython") && len(split(a:text,"\n")) > 1
-  --         return ["%cpaste -q\n", slime#config#resolve("dispatch_ipython_pause"), a:text, "--\n"]
-  --       else
-  --         let empty_lines_pat = '\(^\|\n\)\zs\(\s*\n\+\)\+'
-  --         let no_empty_lines = substitute(a:text, empty_lines_pat, "", "g")
-  --         let dedent_pat = '\(^\|\n\)\zs'.matchstr(no_empty_lines, '^\s*')
-  --         let dedented_lines = substitute(no_empty_lines, dedent_pat, "", "g")
-  --         let except_pat = '\(elif\|else\|except\|finally\)\@!'
-  --         let add_eol_pat = '\n\s[^\n]\+\n\zs\ze\('.except_pat.'\S\|$\)'
-  --         return substitute(dedented_lines, add_eol_pat, "\n", "g")
-  --       end
-  --     endfunction
-  --
-  --   ]]
-  --   end,
-  --   config = function()
-  --     vim.keymap.set({ 'n', 'i' }, '<c-cr>', function()
-  --       vim.cmd [[ call slime#send_cell() ]]
-  --     end, { desc = 'Send code cell to terminal' })
-  --   end,
-  -- },
+  {
+    'jpalardy/vim-slime',
+    enabled = false,
+    init = function()
+      vim.g.slime_target = 'neovim'
+      vim.g.slime_python_ipython = 1
+      vim.g.slime_dispatch_ipython_pause = 100
+      vim.g.slime_cell_delimiter = '#\\s\\=%%'
+      vim.cmd [[
+      function! _EscapeText_quarto(text)
+        if slime#config#resolve("python_ipython") && len(split(a:text,"\n")) > 1
+          return ["%cpaste -q\n", slime#config#resolve("dispatch_ipython_pause"), a:text, "--\n"]
+        else
+          let empty_lines_pat = '\(^\|\n\)\zs\(\s*\n\+\)\+'
+          let no_empty_lines = substitute(a:text, empty_lines_pat, "", "g")
+          let dedent_pat = '\(^\|\n\)\zs'.matchstr(no_empty_lines, '^\s*')
+          let dedented_lines = substitute(no_empty_lines, dedent_pat, "", "g")
+          let except_pat = '\(elif\|else\|except\|finally\)\@!'
+          let add_eol_pat = '\n\s[^\n]\+\n\zs\ze\('.except_pat.'\S\|$\)'
+          return substitute(dedented_lines, add_eol_pat, "\n", "g")
+        end
+      endfunction
+      function! _EscapeText_python(text)
+        if slime#config#resolve("python_ipython") && len(split(a:text,"\n")) > 1
+          return ["%cpaste -q\n", slime#config#resolve("dispatch_ipython_pause"), a:text, "--\n"]
+        else
+          let empty_lines_pat = '\(^\|\n\)\zs\(\s*\n\+\)\+'
+          let no_empty_lines = substitute(a:text, empty_lines_pat, "", "g")
+          let dedent_pat = '\(^\|\n\)\zs'.matchstr(no_empty_lines, '^\s*')
+          let dedented_lines = substitute(no_empty_lines, dedent_pat, "", "g")
+          let except_pat = '\(elif\|else\|except\|finally\)\@!'
+          let add_eol_pat = '\n\s[^\n]\+\n\zs\ze\('.except_pat.'\S\|$\)'
+          return substitute(dedented_lines, add_eol_pat, "\n", "g")
+        end
+      endfunction
+
+    ]]
+    end,
+    config = function()
+      vim.keymap.set({ 'n', 'i' }, '<c-cr>', function()
+        vim.cmd [[ call slime#send_cell() ]]
+      end, { desc = 'Send code cell to terminal' })
+    end,
+  },
 
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
@@ -1039,11 +1040,11 @@ require('lazy').setup({
     end,
   },
   {
-    'GCBallesteros/jupytext.nvim',
+    'YangTack/jupytext.nvim',
     opts = {
-      style = 'markdown',
-      output_extension = 'md',
-      force_ft = 'markdown',
+      style = 'quarto',
+      output_extension = 'ju.qmd',
+      force_ft = 'quarto',
     },
   },
   { -- Highlight, edit, and navigate code
